fe8e01d78a313eae29af2d2fb39b260f7cebc2c1
Lepha <lepharamchiary@gmail.com>
2024-09-09 16:56:57 +0530

    first commit

---
+++ src/pages/Home.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+function Home() {
+    return (
+        <div className="w-full min-h-screen bg-gray-800">
+            <div className="flex flex-wrap mx-10 py-10">
+                <div className="w-full md:w-1/2 border mb-5 md:mb-0">
+                    egbegb
+                </div>
+                <div className="w-full md:w-1/2 border">
+                    <div>
+                        <div className="h-60 md:h-[330px] w-full mb-5">dghdfgf</div>
+                        <div className="h-60 md:h-[330px] w-full">dghdfgf</div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Home;
